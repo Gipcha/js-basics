d7b54b4d1660cbb16d25b262c92e2d03fc39f268
Anna <gipcha2000@gmail.com>
2026-01-25 08:43:00 -0800

    added lesson-7

---
+++ lesson-7/script.js
@@ -0,0 +1,32 @@
+/* 1. Напиши программу, которая использует цикл for, чтобы вывести числа от 1 до 10;*/
+
+for (let i = 0; i <= 100; i++) {
+    console.log (i);
+}
+
+/*2. Напиши программу, которая использует цикл while, чтобы посчитать сумму чисел от 1 до 100;*/
+
+let i = 1
+let sum = 0
+while (i <= 100) {
+    sum = sum + i;
+    i++;
+}
+console.log (sum)
+
+/*3.*Выведи в консоль все простые числа от 0 до 100.*/
+
+for (let number = 2; number <= 100; number++) {
+  let isPrimeNumber = true;
+
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      isPrimeNumber = false;
+      break;
+    }
+  }
+
+  if (isPrimeNumber) {
+    console.log(number);
+  }
+}
